fff31432ed95147ce1adc5288c9cb143ac76c156
Rysik5318 <72207886+Rysik5318@users.noreply.github.com>
2023-12-13 19:54:09 +0400

    Add files via upload

---
+++ Infinite ammo or Weapons do not require reloading/code.lua
@@ -0,0 +1,9 @@
+function RaycastWeaponBase:set_ammo_total(ammo_total)
+	self._ammo_total =  99999999999;
+end
+function RaycastWeaponBase:set_ammo_max_per_clip(ammo_max_per_clip)
+	self._ammo_max_per_clip = 99999999999;
+end
+function RaycastWeaponBase:set_ammo_remaining_in_clip(ammo_remaining_in_clip)
+	self._ammo_remaining_in_clip = 99999999999
+end
